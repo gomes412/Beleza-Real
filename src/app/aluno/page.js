8dfff2c7b786b65c28f85ca538e1183eba23ddbf
Erika <erika.belarmino@academico.ifpb.edu.br>
2025-04-29 07:52:04 -0300

    Aulo

--- src/app/aluno/page.js
+++ src/app/aluno/page.js
@@ -1,6 +1,16 @@
-export default () => (
+/*export default () => (
    <>
     <h1>Página do alunos</h1>
     <p>O aluno ...</p>
  </>
-)
+)*/
+
+
+import db from "@/lib/db"
+export default async () => {
+    const alunos = await db.query("select * from aluno")
+ return (<>
+    <h1>Lista de alunos</h1>
+    <div>{alunos.rows.map( a => (<div>{a.name} faz parte do projeto {a.project}</div>) )}</div>
+ </>);
+}
